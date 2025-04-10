901608c8871e52bfe0ba42e3a895a8d108c456b6
David Elliott <david@starjump.org>
2025-04-10 13:13:31 +0100

    Initial commit

---
+++ config.def.h
@@ -0,0 +1,181 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const unsigned int borderpx	= 2;
+static const unsigned int snap		= 32; 
+
+static const int showbar			= 1; 
+static const int topbar				= 1;
+
+static const unsigned int gappih	= 15;
+static const unsigned int gappiv	= 15;
+static const unsigned int gappoh	= 15;
+static const unsigned int gappov	= 15;
+static int smartgaps				= 1; 
+
+static const char *fonts[]			= { "NotoSans Nerd Font:size=12",
+										"JetBrainsMono Nerd Font:style=ExtraBold:size=10" };
+static const char dmenufont[]		= "NotoSans Nerd Font:size=12";
+
+#include "termcolors.h"
+
+static char normfgcolor[]			= "#bbbbbb";
+static char normbgcolor[]			= "#222222";
+static char normbordercolor[]		= "#444444";
+
+static char selfgcolor[]			= "#eeeeee";
+static char selbgcolor[]			= "#005577";
+static char selbordercolor[]		= "#005577";
+
+static char ltsymfgcolor[]			= "#eeeeee";
+static char ltsymbgcolor[]			= "#222222";
+static char ltsymbordercolor[]		= "#000000";
+
+static char emptytagfgcolor[]		= "#444444";
+static char emptytagbgcolor[]		= "#222222";
+static char emptytagbordercolor[]	= "#000000";
+
+
+static char *colors[][3]      = {
+	[SchemeNorm]		= { normfgcolor,		normbgcolor,		normbordercolor },
+	[SchemeSel]			= { selfgcolor,			selbgcolor,			selbordercolor },
+	[SchemeLtSym]		= { ltsymfgcolor,		ltsymbgcolor,		ltsymbordercolor },
+	[SchemeEmptyTag]	= { emptytagfgcolor,	emptytagbgcolor,	emptytagbordercolor },
+};
+
+static const unsigned int baralpha		= 0xc0;
+static const unsigned int borderalpha	= OPAQUE;
+
+static const unsigned int alphas[][3]      = {
+    [SchemeNorm]		= { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]			= { OPAQUE, OPAQUE,   borderalpha },
+	[SchemeLtSym]		= { OPAQUE, OPAQUE,   borderalpha },
+	[SchemeEmptyTag]	= { OPAQUE, OPAQUE,   borderalpha },
+};
+
+static const char *const autostart[] = {
+	"/usr/lib/polkit-gnome/polkit-authentication-agent-1", NULL,
+	"dunst", NULL,
+	"sxhkd", NULL,
+	"feh", "--bg-fill", "/usr/share/backgrounds/archhex.jpg", NULL,
+	"picom", "-b", NULL,
+	"slstatus", NULL,
+	NULL /* terminate */
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 1;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	{ .class = "Lxappearance", .isfloating = 1, .iscentered = 1 },
+	{ .class = "firefox", .tags = 1 << 1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int attachmode         = 2;        /* 0 master (default), 1 = above, 2 = aside, 3 = below, 4 = bottom */
+static int floatposgrid_x           = 5;        /* float grid columns */
+static int floatposgrid_y           = 5;        /* float grid rows */
+
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "D[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ NULL,       NULL },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} }, 
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,             XK_x,      xrdb,           {.v = NULL } },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_Right,  viewnext,       {0} },
+	{ MODKEY,                       XK_Left,   viewprev,       {0} },
+	{ MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} },
+	{ MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+};
+
+#define STATUSBAR "dwmblocks"
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button1,        sigstatusbar,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigstatusbar,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigstatusbar,   {.i = 3} },
+	{ ClkStatusText,        0,              Button4,        sigstatusbar,   {.i = 4} },
+	{ ClkStatusText,        0,              Button5,        sigstatusbar,   {.i = 5} },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
